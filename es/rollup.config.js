8238c0726fa14c7b465ed8234e7e2f1b2209dde1
Oleg Nechaev <lega911@gmail.com>
2021-06-12 00:15:40 +0200

    Example for ES, AMD modules

---
+++ es/rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from '@rollup/plugin-node-resolve';
+import derver from 'derver/rollup-plugin';
+import malina from 'malinajs/malina-rollup'
+
+const DEV = !!process.env.ROLLUP_WATCH;
+
+export default {
+    preserveEntrySignatures: false,
+    input: 'src/main.js',
+    output: {
+        dir: 'public/',
+        format: 'es',
+    },
+    plugins: [
+        malina({
+            hideLabel: !DEV
+        }),
+        resolve(),
+        DEV && derver()
+    ],
+    watch: {
+        clearScreen: false
+    }
+}
